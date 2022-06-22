124672e47554b5b4d47e68de3fa45a464bfa483a
yhs <gkftn95@naver.com>
2022-06-22 10:33:09 +0900

    step2dispat

--- src/main/java/notice/controller/MyDispatcher.java
+++ src/main/java/notice/controller/MyDispatcher.java
@@ -16,6 +16,9 @@ import notice.controller.customer.NoticeEditController;
 import notice.controller.customer.NoticeEditProcController;
 import notice.controller.customer.NoticeRegController;
 import notice.controller.customer.NoticeRegProcController;
+import notice.controller.joinus.LoginController;
+import notice.controller.joinus.LoginProcController;
+import notice.controller.joinus.LogoutProcController;
 
 public class MyDispatcher extends HttpServlet{
 	
@@ -50,7 +53,13 @@ public class MyDispatcher extends HttpServlet{
 				controller=new NoticeDelProcController();
 			}else if(com.equals("/customer/notice.do")) {
 				controller=new NoticeController();
-			}				
+			}else if(com.equals("/login/login.do")) {
+				controller=(Controller) new LoginController();
+			}else if(com.equals("/login/loginProc.do")) {
+				controller=(Controller) new LoginProcController();
+			}else if(com.equals("/login/logoutproc.do")) {
+				controller=(Controller) new LogoutProcController();
+			}								
 			controller.execute(request, response);
 		}catch(Exception e) {
 			e.printStackTrace();
